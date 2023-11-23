254374f8d618634ebb853f405e0942a7a3949939
takafumi <tikutaka.dev@gmail.com>
2023-11-23 11:45:50 +0900

    #11 サインアップとサインアウト画面のCSSリファクタリング

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -3,28 +3,26 @@ import Breadcrumbs from "@/components/breadcrumbs";
 
 export default function Page() {
   return (
-    <div className="flex w-full items-center justify-center flex-col">
+    <div className="flex-col-center">
       <Breadcrumbs currentPage="ログアウト" />
-      <main className="w-full h-full">
-        <section className="w-full h-[calc(100vh_-_95px)]">
-          <div className="flex flex-col h-full items-center justify-center px-6 py-8 mx-auto lg:py-0 sm:max-w-md">
-            <div className="w-full bg-white rounded-lg shadow dark:border -mt-9 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <div className="flex flex-col justify-center items-center mx-10">
-                  <form
-                    className="w-full"
-                    action={async () => {
-                      "use server";
-                      await signOut({
-                        redirectTo: "/",
-                      });
-                    }}
-                  >
-                    <button type="submit" className="btn btn-primary w-full">
-                      ログアウト
-                    </button>
-                  </form>
-                </div>
+      <main className="fixed-full-with-headers flex-col-center">
+        <section className="flex-col-center justify-center h-full max-w-xs md:max-w-sm">
+          <div className="card w-full -mt-9">
+            <div className="p-6">
+              <div className="flex-col-center items-center px-10">
+                <form
+                  className="w-full"
+                  action={async () => {
+                    "use server";
+                    await signOut({
+                      redirectTo: "/",
+                    });
+                  }}
+                >
+                  <button type="submit" className="btn btn-primary w-full">
+                    ログアウト
+                  </button>
+                </form>
               </div>
             </div>
           </div>
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -6,43 +6,41 @@ import Breadcrumbs from "@/components/breadcrumbs";
 
 export default function Page() {
   return (
-    <div className="flex w-full items-center justify-center flex-col">
+    <div className="flex-col-center">
       <Breadcrumbs currentPage="会員登録" />
-      <main className="w-full h-full">
-        <section className="w-full h-[calc(100vh_-_95px)]">
-          <div className="flex flex-col h-full items-center justify-center px-6 py-8 mx-auto lg:py-0 sm:max-w-md">
-            <div className="w-full bg-white rounded-lg shadow dark:border -mt-9 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <div className="flex flex-col justify-center items-center mx-10">
-                  <form
-                    className="w-full"
-                    action={async () => {
-                      "use server";
-                      await signIn("line", {
-                        redirectTo: "/account/register",
-                      });
-                    }}
+      <main className="fixed-full-with-headers flex-col-center">
+        <section className="flex-col-center justify-center h-full max-w-xs md:max-w-sm">
+          <div className="card w-full -mt-9">
+            <div className="p-6">
+              <div className="flex-col-center items-center px-10">
+                <form
+                  className="w-full"
+                  action={async () => {
+                    "use server";
+                    await signIn("line", {
+                      redirectTo: "/account/register",
+                    });
+                  }}
+                >
+                  <button
+                    type="submit"
+                    className="w-full flex text-white bg-[#06C755] rounded active:[&>div]:bg-black active:[&>div]:bg-opacity-30"
                   >
-                    <button
-                      type="submit"
-                      className="w-full flex text-white bg-[#06C755] rounded active:[&>div]:bg-black active:[&>div]:bg-opacity-30"
-                    >
-                      <div className="flex flex-row w-full h-full rounded hover:bg-black hover:bg-opacity-10">
-                        <div className="flex h-12 w-12 border-r-2 border-black border-opacity-[0.08]">
-                          <Image src={LineLogo} alt="Line logo" />
-                        </div>
-                        <span className="self-center flex-grow">LINEで会員登録</span>
+                    <div className="flex flex-row w-full h-full rounded hover:bg-black hover:bg-opacity-10">
+                      <div className="flex h-12 w-12 border-r-2 border-black border-opacity-[0.08]">
+                        <Image src={LineLogo} alt="Line logo" />
                       </div>
-                    </button>
-                  </form>
-                </div>
+                      <span className="self-center flex-grow">LINEで会員登録</span>
+                    </div>
+                  </button>
+                </form>
               </div>
             </div>
-            <div className="divider" />
-            <Link className="link link-hover" href="/auth/signin">
-              ログインはこちら
-            </Link>
           </div>
+          <div className="divider" />
+          <Link className="link link-hover" href="/auth/signin">
+            ログインはこちら
+          </Link>
         </section>
       </main>
     </div>
