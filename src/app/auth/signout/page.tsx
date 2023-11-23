3acc277ce0f787e0fa202f91a0b7d5393e721e4c
takafumi <tikutaka.dev@gmail.com>
2023-11-23 10:48:47 +0900

    #11 ログアウトページを作成

---
+++ src/app/auth/signout/page.tsx
@@ -0,0 +1,26 @@
+import Breadcrumbs from "@/components/breadcrumbs";
+
+export default function Page() {
+  return (
+    <div className="flex w-full items-center justify-center flex-col">
+      <Breadcrumbs currentPage="ログアウト" />
+      <main className="w-full h-full">
+        <section className="w-full h-[calc(100vh_-_95px)]">
+          <div className="flex flex-col h-full items-center justify-center px-6 py-8 mx-auto lg:py-0 sm:max-w-md">
+            <div className="w-full bg-white rounded-lg shadow dark:border -mt-9 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <div className="flex flex-col justify-center items-center mx-10">
+                  <form className="w-full">
+                    <button type="submit" className="btn btn-primary w-full">
+                      ログアウト
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
