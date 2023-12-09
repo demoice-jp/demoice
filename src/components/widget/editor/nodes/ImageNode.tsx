709700c54e3b12df4fd3501e07fbb4fe437a0068
TakafumiOtake <tikutaka.dev@gmail.com>
2023-12-09 23:43:43 +0900

    #13 画像の選択とドラッグドロップの実装

--- src/components/widget/editor/nodes/ImageNode.tsx
+++ src/components/widget/editor/nodes/ImageNode.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { useLexicalNodeSelection } from "@lexical/react/useLexicalNodeSelection";
+import { mergeRegister } from "@lexical/utils";
+import clsx from "clsx";
 import {
   $applyNodeReplacement,
+  $getNodeByKey,
+  $getSelection,
+  $isNodeSelection,
+  CLICK_COMMAND,
+  COMMAND_PRIORITY_LOW,
   DecoratorNode,
   DOMConversionMap,
   DOMConversionOutput,
   DOMExportOutput,
   EditorConfig,
+  KEY_DELETE_COMMAND,
   LexicalNode,
   NodeKey,
   SerializedLexicalNode,
@@ -114,8 +124,15 @@ export class ImageNode extends DecoratorNode<React.ReactElement> {
   }
 
   decorate(): React.ReactElement {
-    // eslint-disable-next-line @next/next/no-img-element
-    return <img alt={this.__altText} src={this.__src} width={this.__width} height={this.__height} />;
+    return (
+      <ImageComponent
+        src={this.__src}
+        altText={this.__altText}
+        width={this.__width}
+        height={this.__height}
+        nodeKey={this.getKey()}
+      />
+    );
   }
 }
 
@@ -125,4 +142,77 @@ export function $createImageNode({ src, altText, width, height, key }: ImagePayl
 
 export function $isImageNode(node: LexicalNode | null | undefined): node is ImageNode {
   return node instanceof ImageNode;
+}
+
+function ImageComponent({
+  altText,
+  src,
+  width,
+  height,
+  nodeKey,
+}: {
+  altText: string;
+  src: string;
+  width: number;
+  height: number;
+  nodeKey: NodeKey;
+}) {
+  const [editor] = useLexicalComposerContext();
+  const imageRef = useRef<null | HTMLImageElement>(null);
+  const [isSelected, setSelected, clearSelection] = useLexicalNodeSelection(nodeKey);
+
+  const onDelete = useCallback(
+    (payload: KeyboardEvent) => {
+      if (isSelected && $isNodeSelection($getSelection())) {
+        payload.preventDefault();
+        const node = $getNodeByKey(nodeKey);
+        if ($isImageNode(node)) {
+          node.remove();
+        }
+      }
+      return false;
+    },
+    [isSelected, nodeKey],
+  );
+
+  const onClick = useCallback(
+    (payload: MouseEvent) => {
+      const event = payload;
+
+      if (event.target === imageRef.current) {
+        if (event.shiftKey) {
+          setSelected(!isSelected);
+        } else {
+          clearSelection();
+          setSelected(true);
+        }
+        return true;
+      }
+
+      return false;
+    },
+    [isSelected, setSelected, clearSelection],
+  );
+
+  useEffect(() => {
+    return mergeRegister(
+      editor.registerCommand<MouseEvent>(CLICK_COMMAND, onClick, COMMAND_PRIORITY_LOW),
+      editor.registerCommand(KEY_DELETE_COMMAND, onDelete, COMMAND_PRIORITY_LOW),
+    );
+  }, [editor, onClick, onDelete]);
+
+  return (
+    <div draggable>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        className={clsx(isSelected && "focused")}
+        ref={imageRef}
+        alt={altText}
+        src={src}
+        width={width}
+        height={height}
+        draggable={false}
+      />
+    </div>
+  );
 }
--- src/components/widget/editor/plugins/image-plugin.tsx
+++ src/components/widget/editor/plugins/image-plugin.tsx
@@ -4,14 +4,24 @@ import { $wrapNodeInElement, mergeRegister } from "@lexical/utils";
 import Compressor from "compressorjs";
 import {
   $createParagraphNode,
+  $createRangeSelection,
+  $getSelection,
   $insertNodes,
+  $isNodeSelection,
   $isRootOrShadowRoot,
+  $setSelection,
   COMMAND_PRIORITY_EDITOR,
+  COMMAND_PRIORITY_HIGH,
+  COMMAND_PRIORITY_LOW,
   createCommand,
+  DRAGOVER_COMMAND,
+  DRAGSTART_COMMAND,
+  DROP_COMMAND,
   LexicalCommand,
+  LexicalEditor,
 } from "lexical";
 import { ImagePostError, ImagePostResponse } from "@/app/api/media/content/[id]/image/route";
-import { $createImageNode, ImageNode, ImagePayload } from "@/components/widget/editor/nodes/ImageNode";
+import { $createImageNode, $isImageNode, ImageNode, ImagePayload } from "@/components/widget/editor/nodes/ImageNode";
 
 export type InsertImagePayload = Readonly<ImagePayload>;
 
@@ -130,6 +140,9 @@ export default function ImagePlugin() {
       throw new Error("ImagesPlugin: ImageNode not registered on editor");
     }
 
+    // キャッシュさせる
+    getTransparentImgElement();
+
     return mergeRegister(
       editor.registerCommand<InsertImagePayload>(
         INSERT_IMAGE_COMMAND,
@@ -143,8 +156,171 @@ export default function ImagePlugin() {
         },
         COMMAND_PRIORITY_EDITOR,
       ),
+      editor.registerCommand<DragEvent>(
+        DRAGSTART_COMMAND,
+        (event) => {
+          return onDragStart(event);
+        },
+        COMMAND_PRIORITY_HIGH,
+      ),
+      editor.registerCommand<DragEvent>(
+        DRAGOVER_COMMAND,
+        (event) => {
+          return onDragover(event);
+        },
+        COMMAND_PRIORITY_LOW,
+      ),
+      editor.registerCommand<DragEvent>(
+        DROP_COMMAND,
+        (event) => {
+          return onDrop(event, editor);
+        },
+        COMMAND_PRIORITY_HIGH,
+      ),
     );
   }, [editor]);
 
   return null;
+}
+
+let imgCache: null | HTMLImageElement = null;
+function getTransparentImgElement() {
+  if (imgCache) {
+    return imgCache;
+  }
+  // noinspection SpellCheckingInspection
+  const TRANSPARENT_IMAGE = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+  const img = document.createElement("img");
+  img.src = TRANSPARENT_IMAGE;
+  imgCache = img;
+  return imgCache;
+}
+
+function onDragStart(event: DragEvent) {
+  const node = getImageNodeInSelection();
+  if (!node) {
+    return false;
+  }
+  const dataTransfer = event.dataTransfer;
+  if (!dataTransfer) {
+    return false;
+  }
+  dataTransfer.setData("text/plain", "_");
+  dataTransfer.setDragImage(getTransparentImgElement(), 0, 0);
+  dataTransfer.setData(
+    "application/x-lexical-drag",
+    JSON.stringify({
+      data: {
+        altText: node.__altText,
+        height: node.__height,
+        key: node.getKey(),
+        src: node.__src,
+        width: node.__width,
+      },
+      type: "image",
+    }),
+  );
+
+  return true;
+}
+
+function onDragover(event: DragEvent): boolean {
+  const node = getImageNodeInSelection();
+  if (!node) {
+    return false;
+  }
+  if (!canDropImage(event)) {
+    event.preventDefault();
+  }
+  return true;
+}
+
+function onDrop(event: DragEvent, editor: LexicalEditor): boolean {
+  const node = getImageNodeInSelection();
+  if (!node) {
+    return false;
+  }
+  const data = getDragImageData(event);
+  if (!data) {
+    return false;
+  }
+  event.preventDefault();
+  if (canDropImage(event)) {
+    const range = getDragSelection(event);
+    node.remove();
+    const rangeSelection = $createRangeSelection();
+    if (range !== null && range !== undefined) {
+      rangeSelection.applyDOMRange(range);
+    }
+    $setSelection(rangeSelection);
+    editor.dispatchCommand(INSERT_IMAGE_COMMAND, data);
+  }
+  return true;
+}
+
+function getDragImageData(event: DragEvent): null | InsertImagePayload {
+  const dragData = event.dataTransfer?.getData("application/x-lexical-drag");
+  if (!dragData) {
+    return null;
+  }
+  const { type, data } = JSON.parse(dragData);
+  if (type !== "image") {
+    return null;
+  }
+
+  return data as InsertImagePayload;
+}
+
+function canDropImage(event: DragEvent): boolean {
+  const target = event.target;
+  return !!(
+    target &&
+    target instanceof HTMLElement &&
+    !target.closest("span.content-image") &&
+    target.parentElement &&
+    target.parentElement.closest("div.rich-editor")
+  );
+}
+
+function getImageNodeInSelection(): ImageNode | null {
+  const selection = $getSelection();
+  if (!$isNodeSelection(selection)) {
+    return null;
+  }
+  const nodes = selection.getNodes();
+  const node = nodes[0];
+  return $isImageNode(node) ? node : null;
+}
+
+const getDOMSelection = (targetWindow: Window | null): Selection | null =>
+  window && window.document ? (targetWindow || window).getSelection() : null;
+
+function getDragSelection(event: DragEvent): Range | null | undefined {
+  let range;
+  const target = event.target as null | Element | Document;
+  const targetWindow =
+    target == null
+      ? null
+      : target.nodeType === 9
+        ? (target as Document).defaultView
+        : (target as Element).ownerDocument.defaultView;
+  const domSelection = getDOMSelection(targetWindow);
+  // noinspection JSDeprecatedSymbols
+  if (document.caretRangeFromPoint) {
+    // noinspection JSDeprecatedSymbols
+    range = document.caretRangeFromPoint(event.clientX, event.clientY);
+  } else {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    if (event.rangeParent && domSelection !== null) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      domSelection.collapse(event.rangeParent, event.rangeOffset || 0);
+      range = domSelection.getRangeAt(0);
+    } else {
+      throw Error(`Cannot get the selection when dragging`);
+    }
+  }
+
+  return range;
 }
