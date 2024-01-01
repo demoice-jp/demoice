0136a5cd10dc0b6e91836755155a4eb50e2cd125
TakafumiOtake <tikutaka.dev@gmail.com>
2024-01-01 22:46:46 +0900

    #30 キャッシュにより投稿されたコメントがページ遷移後に表示されない問題の修正

--- src/components/hooks.ts
+++ src/components/hooks.ts
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { ContentContext } from "@/components/contexts";
 
 export function useContentContext() {
@@ -7,4 +7,60 @@ export function useContentContext() {
     throw new Error("ContentContextが見つかりません");
   }
   return contentContext;
+}
+
+export function useInfiniteLoad<T>(getUrl: (index: number, previousPageData: T | null) => string | null) {
+  const fetching = useRef<boolean>(false);
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState<unknown>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = useCallback(() => {
+    if (fetching.current) {
+      return;
+    }
+
+    const url = getUrl(data.length, data.length === 0 ? null : data[data.length - 1]);
+    fetching.current = true;
+    setError(null);
+    setIsLoading(true);
+    if (!url) {
+      fetching.current = false;
+      setIsLoading(false);
+      return;
+    }
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw res;
+        }
+        res.json().then((d: T) => {
+          setData((ds) => [...ds, d]);
+        });
+      })
+      .catch((e) => {
+        setError(e);
+      })
+      .finally(() => {
+        fetching.current = false;
+        setIsLoading(false);
+      });
+  }, [data, getUrl]);
+
+  useEffect(() => {
+    if (data.length === 0 && isLoading) {
+      fetchData();
+    }
+  }, [data, isLoading, fetchData]);
+
+  const readMore = useCallback(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return {
+    data,
+    error,
+    isLoading,
+    readMore,
+  };
 }
